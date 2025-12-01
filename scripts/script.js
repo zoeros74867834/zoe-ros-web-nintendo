404449e91234d7843da641bb70101c849a9db0f3
zoeros74867834 <zoe.ros@hva.nl>
2025-12-01 17:19:17 +0100

    update javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 // JavaScript Document
+// Alle functies hebben deze haakje ()
+//je hebt constant daar blijft de waarde altijd gelijk
+//en bij let kan de waarde wel verandere achter const zet je een logische naam en de = waar je naar toe verwijst
+// Queryselector die is een functie die zoek naar een element in de html hij zoek dus naar jouw vraag en die doe je tussen je haakjes
+// Met het groter dan teken ">" verwijs je naar de direct child binnen een parent.
+// Om een functie te schrijven in javavscript begin je altijd met function
+
 console.log("hi");
+const burger = document.querySelector("header nav > button")
+console.log(burger);
+
+//let 
+// Altijd een funtie een naam geven
+//Dus item staat voor place holder voor voor de functie die we later gaan gebruiken 
+// Camelcase betekent dat je de eerste in het tweede woord in kapitaal schrijft
+// openBurger is een functie aanroepen
+// addEventListner voegt een gebeurtenis toe aan je element wat voor nu burger is
+function openBurger(item){
+    console.log("je hebt me geklikt");
+
+}
+
+burger.addEventListener("click", openBurger)
