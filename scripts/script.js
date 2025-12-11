bee450eb61a705c306e886749e86daf4ee64b31e
zoeros74867834 <zoe.ros@hva.nl>
2025-12-11 07:46:18 +0100

    update css

--- scripts/script.js
+++ scripts/script.js
@@ -26,4 +26,23 @@ burger.addEventListener("click", function(){
     openBurger(menuItem)
     console.log('hij klikt');
 });
-
+/* 
+1. https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/grid-column
+  2. https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/grid-auto-rows
+  3. https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/grid-auto-rows
+  4. https://www.accessibility.nl/kennis/zo-zet-je-skiplinks-voor-een-optimale-navigatie
+  5. https://dlo.mijnhva.nl/content/enforced/690110-FDMCI-CRS-00042507/FED%2025-26%20-%20Blok%202%20-%20Oefening%20typografie%20-%201%20letters.pdf
+  6. https://fonts.google.com/specimen/Alegreya
+  7. https://codepen.io/psbwhznu-the-scripter/pen/ogxxjdV
+  8. https://codepen.io/psbwhznu-the-scripter/pen/yyOyYPw
+  9. https://codepen.io/psbwhznu-the-scripter/pen/XJddjJy
+  10.https://codepen.io/psbwhznu-the-scripter/pen/XJddYGp 
+  11. https://codepen.io/psbwhznu-the-scripter/pen/wBGJGQd
+  12. https://codepen.io/psbwhznu-the-scripter/pen/JoXJNYO
+  13. https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/animation-timing-function
+  14. https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Properties/animation-fill-mode
+  15.https://codepen.io/shooft/pen/KwdZPMP
+  16. https://codepen.io/shooft/pen/XJmEOyb
+  17. https://flexboxfroggy.com/#nl
+  18. https://codepen.io/shooft/pen/MYaXoza
+*/
